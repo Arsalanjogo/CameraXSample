c48ae2f46c2da0a0cffab87fe617b966fa060374
Arsalan <arsalan@jogo.ai>
2021-10-11 16:34:45 +0500

    Camera X application with image preview and take photo feature

---
+++ app/src/main/java/com/example/cameraxvideorecording/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.cameraxvideorecording
+
+import android.Manifest
+
+object Constants {
+    const val TAG="cameraX"
+    const val FILE_NAME="yy-MM-dd"
+    const val REQUEST_CODE_PERMISSION=123
+    val requiredPermission= arrayOf(Manifest.permission.CAMERA)
+
+}
